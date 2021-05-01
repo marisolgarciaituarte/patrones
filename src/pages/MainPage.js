2e8e5fe7fcad57373359a95d66cbda82a1a11105
David Rodriguez Gonzalez <davidrguez_29@hotmail.com>
2021-05-01 18:39:07 -0500

    updated MainPage

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,20 +3,25 @@ import { Link } from 'react-router-dom';
 
 const MainPage = () => {
   return (
-    <main>
+    <>
       <section
         style={{
           width: '100%',
           height: 320,
           backgroundColor: 'rgba(0, 0, 0, 0.1)',
+          marginTop: 64,
         }}
       />
-      <section className="d-flex flex-row flex-wrap justify-content-center">
+      <section className="main-container d-flex flex-row flex-wrap justify-content-center">
         <Link to="/patrones/patron-1">
           <div className="card">
             <img
               alt="patron-1"
               src="https://source.unsplash.com/300x300/?clothing"
+              style={{
+                width: 300,
+                height: 300,
+              }}
             />
             <p>Patron 1</p>
           </div>
@@ -26,6 +31,10 @@ const MainPage = () => {
             <img
               alt="patron-2"
               src="https://source.unsplash.com/300x300/?clothes"
+              style={{
+                width: 300,
+                height: 300,
+              }}
             />
             <p>Patron 2</p>
           </div>
@@ -35,12 +44,16 @@ const MainPage = () => {
             <img
               alt="patron-3"
               src="https://source.unsplash.com/300x300/?clothe"
+              style={{
+                width: 300,
+                height: 300,
+              }}
             />
             <p>Patron 3</p>
           </div>
         </Link>
       </section>
-    </main>
+    </>
   );
 };
 
